828e570e8c85048029b3e7107f594f3696acc6cf
Koya Adetoun <adetounkoya1@gmail.com>
2022-12-10 13:51:47 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 
-/**                                                                                                          
- *  main - Entry point       
- *                                                                                
- * Return: Always 0 (Success)                                                                               
- */                                                                                                          
-int main(void)                                                                                               
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
 {
 	printf("\"With ptoper grammar, but the outcome is a piece of art");
 	return (0);
 
-}	
+}
