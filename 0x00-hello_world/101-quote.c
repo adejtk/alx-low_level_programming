e96bc5ed46bdab51a2614092a165ce4c251b5aca
Koya Adetoun <adetounkoya1@gmail.com>
2022-12-10 15:42:27 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
- * main -Entry
- * Return: Always 1
- */
-int main(void)
-{
-write(1. "and that piece of arts is useful\" - Dora Korpar, 2015- 10- 19\n",59);
-return (1);
-}
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
