5d5fa654e814d129a08c3b879c29bf2fe313b86c
Koya Adetoun <adetounkoya1@gmail.com>
2022-12-11 15:49:26 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - generate a random number
+ * main - main function to generate a random number
  *
  * Return: Always 0 (Success)
  *
@@ -11,8 +11,10 @@
 
 int main(void)
 {
+
 	int n;
 	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -22,7 +24,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
