073a94a56af87955646646f3f11b823e1b544aca
Koya Adetoun <adetounkoya1@gmail.com>
2022-12-12 14:15:54 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
