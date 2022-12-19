63e836bc13f7d761d4ca3c266d7aa982e19aae37
Koya Adetoun <adetounkoya1@gmail.com>
2022-12-19 10:26:46 -0800

    strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,10 +18,11 @@ void rev_string(char *s)
 	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
+	{
 		counter--;
 	rev = s[i];
 	s[i] = s[counter];
 	s[counter] = rev;
-}
+	}
 
 }
