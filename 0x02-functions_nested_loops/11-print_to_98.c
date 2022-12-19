5c31f0bd875f3b843792ece618999e83b5bd6712
Koya Adetoun <adetounkoya1@gmail.com>
2022-12-19 11:54:14 -0800

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,18 +20,15 @@ void print_to_98(int n)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
-			{
-				if (n >= 98)
-			{
-				for (j = n; j >= 98; j--)
-				{
-					if (j != 98)
-						printf("%d, ", j);
-					else if (j == 98)
-						printf("%d\n", j);
-				}
-			}
 		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
 }
